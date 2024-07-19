261e721950c7d208d04335da28b867a5a39ac900
Aleksei Ponyatov <ponyatov94@gmail.com>
2024-07-19 06:39:57 +0300

    update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,6 +12,7 @@ return {
     config = function()
 
         local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '-', ":Telescope file_browser<CR>")
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
@@ -21,6 +22,8 @@ return {
         vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
         vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
         vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, {})
+        vim.keymap.set('n', '<leader>fx', builtin.treesitter, {})
+        vim.keymap.set('n', '<leader>fs', builtin.spell_suggest, {})
 --        vim.keymap.set('n', 'gr', builtin.lsp_references,
 --                    {noremap = true, silent = true})
         vim.keymap.set('n', 'gd', builtin.lsp_definitions,

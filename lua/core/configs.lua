ff774bc57c1a0e93403dfd25b11defcf132e84e4
Aleksei Ponyatov <ponyatov94@gmail.com>
2025-04-05 20:41:38 +0300

    tab 4 space

--- lua/core/configs.lua
+++ lua/core/configs.lua
@@ -43,9 +43,9 @@ vim.opt.shortmess:append("c")
 
 -- Indent Settings
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.autoindent = true
 
 -- Fillchars
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -59,6 +59,7 @@ return {
                     leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
                 },
 
+
                 filtered_items = {
                     visible = true,
                     show_hidden_coutn = true,
